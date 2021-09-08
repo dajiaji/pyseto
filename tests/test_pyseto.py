d87e2d2adfdeca0a6366b8bd71efe861b949de06
Ajitomi, Daisuke <ajitomi@gmail.com>
2021-09-09 05:59:58 +0900

    Add pyseto.encode and decode test.

--- tests/test_pyseto.py
+++ tests/test_pyseto.py
@@ -58,3 +58,28 @@ class TestPyseto:
             pyseto.decode(k, version + ".public.11111111")
             pytest.fail("pyseto.decode() should fail.")
         assert msg in str(err.value)
+
+    @pytest.mark.parametrize(
+        "version, public_key",
+        [
+            ("v1", load_key("keys/public_key_rsa.pem")),
+            ("v2", load_key("keys/public_key_ed25519.pem")),
+            ("v3", load_key("keys/public_key_ecdsa_p384.pem")),
+        ],
+    )
+    def test_decode_with_another_version_key(self, version, public_key):
+        sk = Key.new("v4", "public", load_key("keys/private_key_ed25519.pem"))
+        pk = Key.new(version, "public", public_key)
+        token = pyseto.encode(sk, "Hello world!")
+        with pytest.raises(ValueError) as err:
+            pyseto.decode(pk, token)
+            pytest.fail("pyseto.decode() should fail.")
+        assert "key is not found for verifying the token." in str(err.value)
+
+    def test_decode_with_empty_list_of_keys(self):
+        sk = Key.new("v4", "public", load_key("keys/private_key_ed25519.pem"))
+        token = pyseto.encode(sk, "Hello world!")
+        with pytest.raises(ValueError) as err:
+            pyseto.decode([], token)
+            pytest.fail("pyseto.decode() should fail.")
+        assert "List[KeyInterface] for keys is not supported so far." in str(err.value)
