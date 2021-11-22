08b1245b39057d352341095de2c009db51d731e3
Ajitomi, Daisuke <ajitomi@gmail.com>
2021-11-22 23:44:36 +0900

    Bump up version to v1.4.0.

--- pyseto/__init__.py
+++ pyseto/__init__.py
@@ -12,7 +12,7 @@ from .paseto import Paseto
 from .pyseto import decode, encode
 from .token import Token
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 __title__ = "PySETO"
 __description__ = "A Python implementation of PASETO"
 __url__ = "https://pyseto.readthedocs.io"
